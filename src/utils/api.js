de7e7ef6fe8701ad861e7cf923bb0ce334134422
plake <plake492@gmail.com>
2020-05-14 12:57:51 -0400

    setting up files

---
+++ src/utils/api.js
@@ -0,0 +1,15 @@
+const baseURL = "https://apidemo.fattlabs.com/";
+
+exports.default = {
+  getPastInvoices: function () {
+    return fetch(`${baseURL}item`, {
+      method: "get",
+      headers: new Headers({
+        Authorization:
+          "Bearer" +
+          "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZXJjaGFudCI6IjZkMzI5Nzk3LWI2NGQtNDdkMS1hNDU3LTQ3OThlMmIzNjFiNSIsImdvZFVzZXIiOmZhbHNlLCJzdWIiOiI0ODI3ODAzNi1jYzE5LTQ3YWItYTgyOC03MzRiOWUxYWNlMDIiLCJpc3MiOiJodHRwOi8vYXBpZGVtby5mYXR0bGFicy5jb20vdGVhbS9hcGlrZXkiLCJpYXQiOjE1NTU0Mjg2MzMsImV4cCI6NDcwOTAyODYzMywibmJmIjoxNTU1NDI4NjMzLCJqdGkiOiJxZTNueklKdWlDOFhKN1dhIiwiYXNzdW1pbmciOmZhbHNlfQ.TEmlwmgVBLwt5x0FO4c-mbY3JgO_tgxcFRfznlOGSrM",
+        "Content-Type": "application/json",
+      }),
+    });
+  },
+};
